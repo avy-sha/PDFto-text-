ad687dc764b911c8eb95b36e4ea76ff08368bfb7
abhinav sharma <abhinav2061997@gmail.com>
2018-02-25 16:43:33 +0530

    first

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var pdfUtil = require('pdf-to-text');
+var pdf_path = "./Junagadh.pdf";
+var app = express();
+var fs = require('fs');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+//option to extract text from page 0 to 10
+var option = {from: 0, to: 10};
+
+pdfUtil.pdfToText(pdf_path, option, function(err, data) {
+	if (err) throw(err);
+	fs.writeFile("./output.txt", data, function(err) {
+		if(err) {
+			return console.log(err);
+		}
+      }); //print text
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+
+
+});
+
+module.exports = app;
